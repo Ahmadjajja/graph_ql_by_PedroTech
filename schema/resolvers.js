94bc6d690371f487c91099b760fd86fb8c03d02c
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2022-12-14 17:24:38 +0500

    Graphql Query and Mutation Concept done...Video scene upto 2:23:00  min

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -6,36 +6,72 @@ const resolvers = {
     // users: () =>  {     // same as below line
     users() {
       //When we require users from frontend , This will return the exactly data that we want.
+      // console.log("UserList", UserList);
       return UserList;
     },
-    user: (parent, args) => {
-      //dealing with different arguments in resolvers
+
+
+    user : (parent, args) =>  {
+      // console.log('args.id');    //why this is not working
       const id = args.id; //accessing id
-      const user = _.find(UserList, { id: Number(id) });
-    //   console.log('====================================');
-    //   console.log(user);
-    //   console.log('====================================');
+      const user = _.find(UserList, { id: (Number(id)) });
       return user;
     },
+
+
+
     //MOVIE RESOLVERS
     movies: () => {
       return MovieList;
     },
+
+
     movie: (parent, args) => {
       const name = args.name;
       const movie = _.find(MovieList, { name });
       return movie;
     },
-    user: {
-      favoriteMovies: () => {
-        return _.filter(
-          MovieList,
-          (movie) =>
-            movie.yearOfPublication >= 2007 && movie.yearOfPublication <= 2009
-        );
-      },
+
+
+    // user: {
+    //   favoriteMovies: () => {
+    //     return _.filter(
+    //       MovieList,
+    //       (movie) =>
+    //         movie.yearOfPublication >= 2007 && movie.yearOfPublication <= 2009
+    //     );
+    //   },
+    // },
+  },
+  
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = args.input;
+      console.log(user);
+      const lastId = UserList[UserList.length-1].id
+      user.id = lastId + 1;
+      UserList.push(user);
+      return user
     },
-  }, 
+    updateUsername: (parent, args) => {
+      const { id, newUsername } = args.input;
+      let userUpdated;
+      UserList.forEach((user) => {
+        if ( user.id === Number(id)){
+          user.username = newUsername;
+          userUpdated = user;
+        }
+      })
+      return userUpdated
+    },
+
+    deleteUser: (parent, args) => {
+      const id = args.id;
+      _.remove(UserList, (user) => user.id  === Number(id));
+      return null;
+    }
+  }
+
 };
 
 module.exports = { resolvers };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -2,58 +2,62 @@ const { gql } = require("apollo-server");
 
 //One confusion is assigning type to another query field is similar like aggrigation
 
-const typeDefs = gql`  
-    type User { 
-        id: ID!
-        name: String!
-        username: String!
-        age: Int! 
-        nationality: Nationality!
-        friends: [User]
-        favoriteMovies: [Movie!]
-    }
+const typeDefs = gql`
+  type User {
+    id: ID
+    name: String
+    username: String
+    age: Int
+    nationality: Nationality
+    friends: [User]
+    favoriteMovies: [Movie!]
+  }
 
-    type Movie {
-        id: ID!
-        name: String!
-        yearOfPublication: Int!
-        isInTheaters: Boolean!
-    }
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
+  }
 
-    type Query {
-        users: [User!]!
-        user(id: ID!): User!  
-        movies: [Movie!]!
-        movie(name: String!): Movie!
-    }
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
+
+  enum Nationality {
+    CANADA
+    BRAZIL
+    INDIA
+    GERMANY
+    CHILE
+    TOGO
+    MONACO
+  }
+
+  input CreateUserInput {
+    name: String!
+    username: String!
+    age: Int!
+    nationality: Nationality = BRAZIL
+  }
+
+  input updateUsernameInput {
+
+    id: ID! 
+    newUsername: String!
+
+  }
 
 
 
-    enum Nationality {
-        CANADA
-        BRAZIL
-        INDIA
-        GERMANY
-        CHILE
-        TOGO
-        MONACO
-    }
-
-
+  type Mutation {
+    createUser(input: CreateUserInput!): User
+    updateUsername(input: updateUsernameInput!): User
+    deleteUser(id: ID!): User
+  }
 `;
-
-// input CreateUserInput {
-//     name: String!
-//     username: String!
-     
-//     age: Int! 
-//     nationality: Nationality!
-//     friends: [User]
-//     favoriteMovies: [Movie!]
-// }
-
-// type Mutation {
-//     createUser(input: CreateUserInput!): User!
-// }
 
 module.exports = { typeDefs };
